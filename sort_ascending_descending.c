0ddc30e3a4e36231c859de7fb0589d68d1ab84e5
shabbnam <shabrnd@gmail.com>
2015-12-27 02:12:15 +0530

    committed

--- sort_ascending_descending.c
+++ sort_ascending_descending.c
@@ -15,7 +15,7 @@ struct node  //node structure
 
 int main()
 {
-    int data=0;
+    int data=0,nodecount=0;
    struct node *head=NULL;
    printf("enter -1 to stop \n \n ");
    while(data!=-1)
@@ -26,14 +26,38 @@ int main()
       scanf("%d",&data);
       if(data==-1)break;
       head=insert_node(head,data);
+      nodecount++;
+
 
   }
+  if(head==NULL)
+  {
+      printf("list empty");
+      exit(0);
+  }
+  if(nodecount==1)// to handle the sample  input :2->null
+  {
+      display(head);
+
+  }
+  else if(nodecount==2)//to handle the  sample input :2 ->1->null
+  {
+      if(head->data>head->next->data)
+      {
+          data= head->data;
+          head->data=head->next->data;
+          head->next->data=data;
+      }
+       display(head);
+  }
+  else{
   display(head);
   head=sort_list(head);
   printf("\n");
   printf(" sorted :");
   display(head);
     return 0;
+}
 }
 struct node *insert_node(struct node *head,int  data) //insert node at last
 {
@@ -63,6 +87,7 @@ struct node *sort_list(struct node *head)//sort list
     struct node *q,*breakptr,*p=head,*temp,*t;
     int flag=1;
     while(p->data<p->next->data){p=p->next;}
+
     breakptr=p->next;// points to the node from where descending order starts
     while(breakptr)// runs till it becomes null
     {
